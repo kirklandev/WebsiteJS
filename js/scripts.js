95436c696176ca80abc6f9c27bd70732d76cd530
Evan Kirkland <ekirkland@credera.com>
2016-09-15 10:27:23 -0500

    Finished Contact page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$("#MaterialsLink, #StepsLink, #TipsLink").click(function(){
+$("#MaterialsLink, #StepsLink, #TipsLink, #MediaLink").click(function(){
     var idName = $(this).attr('id');
     switch(idName){
         case 'MaterialsLink':
             $("#Materials").css({
                       transition: 'background-color 1s ease-in-out',"background-color": "aliceblue"
             });
-            $("#Tips, #Steps").css({
+            $("#Tips, #Steps, #Media").css({
                       transition: 'background-color 1s ease-in-out',"background-color": "white"
             });
             break;
@@ -13,7 +13,7 @@ $("#MaterialsLink, #StepsLink, #TipsLink").click(function(){
             $("#Steps").css({
                       transition: 'background-color 1s ease-in-out',"background-color": "aliceblue"
             });
-            $("#Tips, #Materials").css({
+            $("#Tips, #Materials, #Media").css({
                       transition: 'background-color 1s ease-in-out',"background-color": "white"
             });
             break;
@@ -21,7 +21,15 @@ $("#MaterialsLink, #StepsLink, #TipsLink").click(function(){
             $("#Tips").css({
                       transition: 'background-color 1s ease-in-out',"background-color": "aliceblue"
             });
-            $("#Materials, #Steps").css({
+            $("#Materials, #Steps, #Media").css({
+                      transition: 'background-color 1s ease-in-out',"background-color": "white"
+            });
+            break;
+        case 'MediaLink':
+            $("#Media").css({
+                      transition: 'background-color 1s ease-in-out',"background-color": "aliceblue"
+            });
+            $("#Materials, #Steps, #Tips").css({
                       transition: 'background-color 1s ease-in-out',"background-color": "white"
             });
             break;
